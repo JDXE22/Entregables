3d3c7978f53154c7fd42176d66f12ff4693de46b
JDEX22 <davidesparzaj22@gmail.com>
2026-07-17 16:05:02 -0500

    feat: Añadir menú interactivo para la gestión de citas y registros

---
+++ JDSafe/interfaces/menu.py
@@ -0,0 +1,41 @@
+print("Bienvenido al programa JDSafe \n")
+print("A continuacion se va a desplegar un menu que funciona con numeros \n")
+print("Si ingresa una opcion incorrecta, el menu se desplegara otra vez\n")
+
+while True: 
+    try:
+        print("1. Programar citas ")
+        print("2. Asistencias y observaciones ")
+        print("3. Registrar clientes")
+        print("4. Registrar instructor")
+        print("5. Registrar vehiculo")
+        print("6. Consultar citas agendadas")
+        print("7. Consultar historial del cliente")
+        print("Ingrese 0 para terminar las votaciones y ver los resultados finales")
+        opcion = int(input("Ingrese la opcion: \n"))    
+        
+    except TypeError as e:
+        print("Se ha ingresado una opcion no valido\n")
+    except Exception as err: 
+        print(f"Se ha presentado un error inesperado {err}\n")
+
+    else:
+        if opcion == 1: 
+            print("Se esta programando una cita \n")
+        elif opcion == 2:
+            print("Registro de asistencia y observaciones \n")
+        elif opcion == 3:
+            print("Registro de cliente \n")
+        elif opcion == 4:
+                print("Registro de instructor \n")
+        elif opcion == 5:
+                print("Registro de vehiculos \n")
+        elif opcion == 6:
+                print("Consultar citas \n")
+        elif opcion == 7:
+                print("Consultar historial del cliente \n")
+        elif opcion == 0:
+            print("Saliendo de el programa, vuelva pronto... \n")
+            break
+
+
