6b00d9bff9da0e7c1e880aee7f1f4cc561130b24
JuanDavid <davidesparzaj22@gmail.com>
2026-07-20 16:02:27 -0500

    feat: Mejorar la gestión de asistencias y observaciones con correcciones en la lógica de entrada

--- JDSafe/interfaces/asistencias.py
+++ JDSafe/interfaces/asistencias.py
@@ -1,13 +1,49 @@
-asistencia_y_observaciones = {}
+from datetime import datetime
+asistencia_y_observaciones = []
 print("Bienvenido al apartado de asistencias y observaciones \n")
 print("A continuacion se va a desplegar un menu que funciona con numeros \n")
 
 while True:
-  print("1. Registrar asistencia y observacion")
-  print("2. Consultar asistencia y observacion")
-  print("Ingrese 0 para salir del apartado de asistencias y observaciones. Esto lo llevara al menu principal \n")
+  try:
+      print("1. Registrar asistencia y observacion")
+      print("2. Consultar asistencia y observacion")
+      print("Ingrese 0 para salir del apartado de asistencias y observaciones. Esto lo llevara al menu principal \n")
 
-  opcion = int(input("Ingrese la opcion: \n"))
+      opcion = int(input("Ingrese la opcion: \n"))
+      
+      if opcion == 1:
+        cita = input("Ingrese el codigo de la cita")
+        fecha = datetime.now().strftime("%d/%m/%Y %H:%M:%S").date()
+        for record in asistencia_y_observaciones.values():
+            if cita["codigo"] == cita:
+              observacion = input("Inserte la observacion de la clase")
+              resultado = {f"Codigo:{cita}, Fecha:{fecha}, Observacion:{observacion}"}
+              asistencia_y_observaciones.append(resultado)
+              print("Asistencia y observacion registrada con exito")
+      
+      elif opcion == 2:
+        cita = input("Ingrese el codigo de la cita")
+        for asistencia in asistencia_y_observaciones:
+          if cita["codigo"] == cita:
+            print(asistencia_y_observaciones)
+            
+      elif opcion == 3:
+        print("Saliendo del apartado de asistencias y observaciones")
+        break
+    
+  except ValueError:
+      print("Opcion invalida. Por favor, ingrese un numero valido.")
+  
+  except KeyError:
+      print("Codigo de cita no encontrado. Por favor, ingrese un codigo valido.")
+  
+  except Exception as e:
+      print(f"Ocurrio un error inesperado: {e}")
+    
+            
+              
+        
+
     
 
 
