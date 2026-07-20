2daadcda8390a8f9a7aacc480b4f2b8e82fc26b9
JuanDavid <davidesparzaj22@gmail.com>
2026-07-20 08:23:02 -0500

    feat: Añadir menú para registrar y consultar asistencias y observaciones

---
+++ JDSafe/interfaces/asistencias.py
@@ -0,0 +1,13 @@
+asistencia_y_observaciones = {}
+print("Bienvenido al apartado de asistencias y observaciones \n")
+print("A continuacion se va a desplegar un menu que funciona con numeros \n")
+
+while True:
+  print("1. Registrar asistencia y observacion")
+  print("2. Consultar asistencia y observacion")
+  print("Ingrese 0 para salir del apartado de asistencias y observaciones. Esto lo llevara al menu principal \n")
+
+  opcion = int(input("Ingrese la opcion: \n"))
+    
+
+
