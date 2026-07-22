579b00a29ab84c70d0ae4a5e7704dc45797aeccb
JuanDavid <davidesparzaj22@gmail.com>
2026-07-22 07:40:56 -0500

    refactor: Refactorizado de las funciones consultar citas  e implementacion de manejo de citas

--- JDSafe/interfaces/citas.py
+++ JDSafe/interfaces/citas.py
@@ -9,43 +9,50 @@ def agendar_cita():
     vehiculo = input("Seleccione el tipo de vehiculo")
     fecha_insertada = input("Ingrese en formato DD/MM/YY la fecha de la cita")
     hora_insertada = input("Ingrese en formato HH:MM la hora de la cita")
-    fecha = datetime.strptime(fecha_insertada, "%d/%m/%y").date()
-    hora = datetime.strptime(hora_insertada, "%H:%M").time()
+    fecha_f = datetime.strptime(fecha_insertada, "%d/%m/%y").strftime("%d/%m/%y")
+    hora_f = datetime.strptime(hora_insertada, "%H:%M").strftime("%H:%M")
     
     cita = {
       "cliente": cliente,
       "instructor": instructor,
       "vehiculo": vehiculo,
-      "fecha": fecha,
-      "hora": hora
+      "fecha": fecha_f,
+      "hora": hora_f
     }
 
     citas.append(cita)
+    funciones.crear_archivo_txt("citas_clientes", cita)
 
-    with open("citas_clientes.txt", "w") as archivo:
-      for cita in citas:
-        archivo.write(f"Cliente: {cita['cliente']}, Instructor: {cita['instructor']}, Vehiculo: {cita['vehiculo']}, Fecha: {cita['fecha']}, Hora: {cita['hora']}\n")
-        print("Se ha agendado la cita correctamente \n")
-
-def consultar_citas_por_cliente(cliente):
-   citas_cliente = []
-   existe = funciones.leer_archivo_txt("citas_clientes")
-   for cita in existe:
-      if cita["cliente"] == cliente:
-        print(f"Se ha encontrado los siguientes resultados del cliente: {cita['cliente']}")
-        citas_cliente.append(cita)
-      return citas_cliente
-    
-def consultar_citas_por_fecha():
-  fecha = input("Ingresar la fecha de la cita programada \n")
-  fecha_f = datetime.strptime(fecha, "%d/%m/%y").date()
-  citas_instructor = []
+def consultar_citas_por_cliente():
+  cliente = int(input("Ingresar numero de documento de 10 digitos del cliente, sin comas o espacios. \n"))
+  citas_cliente = []
   existe = funciones.leer_archivo_txt("citas_clientes")
   for cita in existe:
-      if cita['fecha'] == fecha_f:
-        print(f"Se ha encontrado los siguientes resultados de la fecha: {cita['fecha']} \n")
-        citas_instructor.append(cita)
-      return citas_instructor
+      if cita['cliente'] == cliente:
+        print(f"Se ha encontrado los siguientes resultados del cliente: {cita['cliente']}")
+        print(f"Instructor: {cita['instructor']}, Vehiculo: {cita['vehiculo']}, Fecha: {cita['fecha']}, Hora: {cita['hora']} \n")
+        citas_cliente.append(cita)
+  return citas_cliente
+    
+def consultar_citas_por_fecha():
+  try:
+    fecha = input("Ingresar la fecha de la cita programada \n")
+    fecha_f = datetime.strptime(fecha, "%d/%m/%y").strftime("%d/%m/%y")
+    citas_instructor = []
+    existe = funciones.leer_archivo_txt("citas_clientes")
+    for cita in existe:
+        if cita['fecha'] == fecha_f:
+          print(f"Se ha encontrado los siguientes resultados de la fecha: {cita['fecha']} \n")
+          print(f"Cliente: {cita['cliente']}, Instructor: {cita['instructor']}, Vehiculo: {cita['vehiculo']}, Hora: {cita['hora']} \n")
+          citas_instructor.append(cita)
+    return citas_instructor
+  
+  except ValueError:
+    print("El formato de la fecha ingresada no es valido, por favor ingrese la fecha en el formato DD/MM/YY \n")
+    
+  except Exception as e:
+    print(f"Se ha presentado un error inesperado {e}\n")
+    
 
 def menu_citas():
   print("Bienvenido a la interfaz de citas de JDSafe\n")
@@ -61,15 +68,14 @@ def menu_citas():
       if opcion == 1:
         agendar_cita()
       if opcion == 2:
-        cliente = int(input("Ingresar numero de documento de 10 digitos del cliente, sin comas o espacios. \n"))
-        consultar_citas_por_cliente(cliente)
+        consultar_citas_por_cliente()
       if opcion == 3:
         consultar_citas_por_fecha()        
       if opcion == 0:
         print("Saliendo de el programa, vuelva pronto... \n")
         break
     except TypeError as e:
-      print("Se ha ingresado una opcion no valido\n")
+      print(f"Se ha ingresado un dato no valido {e}\n")
       
     except Exception as e:
       print(f"Se ha presentado un error inesperado {e}\n")
