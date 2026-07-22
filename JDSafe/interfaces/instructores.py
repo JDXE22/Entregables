890531df825156b2df3239f54eaa9b0725b60199
JuanDavid <davidesparzaj22@gmail.com>
2026-07-22 08:05:08 -0500

    feat: Implementar formulario para la creación de instructores con validación de datos

---
+++ JDSafe/interfaces/instructores.py
@@ -0,0 +1,93 @@
+from ..helpers import funciones_txt as funciones
+
+print("Bienvenido al sistema de creacion de instructores")
+print("Dentro de este formulario debe de ingresar los siguientes datos")
+print("1. Primer nombre y primer apellido")
+print("2. Numero de documento")
+print("3. Seleccionar especialidad de enseñanza (Moto, carro o ambos)")
+
+
+while True:
+        nombre = (input("Ingrese su primer nombre y primer apellido: \n"))
+        sin_espacios=nombre.replace(" ","")
+        validacion = sin_espacios.isalpha()
+        if validacion == True:
+             resultado = funciones.calcular_tamaño(nombre)
+             print(resultado)
+             print(f"El nombre es valido y tiene un tamaño de {resultado}")
+             if (6 <= resultado <= 50):
+                    nombre.upper()
+                    print(f"Bienvenido, instructor {nombre}")
+                    break
+             else: 
+                print(f"El nombre tiene solo letras, pero su tamaño ({resultado}) no está entre 6 y 50 caracteres.")
+                print("Intente de nuevo.\n")
+        else:
+            print("El nombre ingresado no fue valido, no debe contener numeros ni simbolos.")
+            print("Debe ser ingresado nuevamente.\n")    
+
+
+
+while True:
+        documento = (input("Ingresar numero de documento de 10 Digitos, sin comas o espacios. \n"))
+        sin_espacios=documento.replace(" ","")
+        validacion = sin_espacios.isdigit()
+        if validacion == True:
+                resultadoD = funciones.calcular_tamaño(documento)
+                print(resultadoD)
+                print(f"El documento tiene un tamaño de {resultadoD} digitos")
+
+                if (resultadoD == 10):
+                        print(f"Documento valido: {documento}")
+                        print("Saliendo")
+                        break
+
+                else:
+                        print(f"El Documento tiene solo numeros, pero su tamaño ({resultadoD}) no es de 10 numeros")
+                        print("Intente de nuevo con un documento valido.\n")
+        else:
+                print("El Documento ingresado no fue valido, no debe contener letras ni simbolos.")
+                print("Debe ser ingresado nuevamente.\n") 
+       
+
+while True:
+        try:
+                print("Seleccione especialidad de enseñanza \n")
+                print("1. Moto")
+                print("2. Carro")
+                print("3. Ambos")
+                opcion = int(input())
+                if opcion == 1:
+                        print("Su especialidad es Moto")
+                        sel = "Moto"
+                        
+                elif opcion == 2:
+                        print("Su especialidad es Carro")
+                        sel = "Carro"
+                        
+                elif opcion == 3:
+                        print("Su especialidad es Ambos (Carro y Moto)")
+                        sel = "Ambos (Carro y Moto)"
+                else:
+                       print("No fueron ingresadas ninguna de las opciones validas")
+
+                print(f"El curso seleccionado de enseñanza fue {sel}")
+                print("Ingrese 0 para confirmar y salir de el programa")
+                sal = int(input())   
+                if sal == 0:
+                       print("Saliendo")
+                       break
+                else:
+                       print("Confirmación cancelada, Iniciando nuevamente menú de especialidad de enseñanza")
+        except ValueError as e:
+               print(f"El dato ingresado no es valido, por lo que genera un error {e}")
+
+instructor_Nuevo = (
+       "Registro de instructor Nuevo \n"
+       f"Nombre: {nombre} \n"
+       f"Documento: {documento} \n"
+       f"Especialidad: {sel} \n"
+       "\n\n\n"
+)
+
+funciones.crear_archivo_txt("data/instructores", instructor_Nuevo)
