752226fe4a613fde5a48c4482c8d4d497f729d05
JuanDavid <davidesparzaj22@gmail.com>
2026-07-22 08:30:19 -0500

    feat: Mejorar la función de agendar cita con validación de vehículo y manejo de excepciones

--- JDSafe/interfaces/citas.py
+++ JDSafe/interfaces/citas.py
@@ -4,25 +4,42 @@ from ..helpers import funciones_txt as funciones
 citas = funciones.leer_archivo_txt("citas_clientes")
 
 def agendar_cita():
-    cliente = int(input("Ingresar numero de documento de 10 digitos del cliente, sin comas o espacios. \n"))
-    instructor = input("Seleccione el instructor con el que desea tener la clase")
-    vehiculo = input("Seleccione el tipo de vehiculo")
-    fecha_insertada = input("Ingrese en formato DD/MM/YY la fecha de la cita")
-    hora_insertada = input("Ingrese en formato HH:MM la hora de la cita")
-    fecha_f = datetime.strptime(fecha_insertada, "%d/%m/%y").strftime("%d/%m/%y")
-    hora_f = datetime.strptime(hora_insertada, "%H:%M").strftime("%H:%M")
+  while True:
+    try:
+      cliente = int(input("Ingresar numero de documento de 10 digitos del cliente, sin comas o espacios. \n"))
+      instructor = input("Seleccione el instructor con el que desea tener la clase")
+      vehiculo = input("Seleccione el tipo de vehiculo: 1. Moto 2. Carro")
+      if vehiculo == "1":
+          vehiculo = "Moto"
+      elif vehiculo == "2": 
+          vehiculo = "Carro"
+      else: 
+          print("Opcion de vehiculo no valida, por favor ingrese una opcion valida")
+          return
+      fecha_insertada = input("Ingrese en formato DD/MM/YY la fecha de la cita")
+      hora_insertada = input("Ingrese en formato HH:MM la hora de la cita")
+      fecha_f = datetime.strptime(fecha_insertada, "%d/%m/%y").strftime("%d/%m/%y")
+      hora_f = datetime.strptime(hora_insertada, "%H:%M").strftime("%H:%M")
+      
+      cita = {
+        "cliente": cliente,
+        "instructor": instructor,
+        "vehiculo": vehiculo,
+        "fecha": fecha_f,
+        "hora": hora_f
+      }      
+      funciones.crear_archivo_txt("citas_clientes", cita)
+      print("Cita agendada correctamente")
+
+    except ValueError:
+      print("El dato ingresado no es valido, por favor ingrese un dato valido \n")
+      
+    except TypeError as e:
+      print(f"Se ha ingresado un dato no valido {e}\n")
+      
+    except Exception as e:
+      print(f"Se ha presentado un error inesperado {e}\n")
     
-    cita = {
-      "cliente": cliente,
-      "instructor": instructor,
-      "vehiculo": vehiculo,
-      "fecha": fecha_f,
-      "hora": hora_f
-    }
-
-    citas.append(cita)
-    funciones.crear_archivo_txt("citas_clientes", cita)
-
 def consultar_citas_por_cliente():
   try:
     cliente = int(input("Ingresar numero de documento de 10 digitos del cliente, sin comas o espacios. \n"))
