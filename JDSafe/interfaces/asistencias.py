1013bbccf968bb7393bc7e1b358ac5f361411f62
JuanDavid <davidesparzaj22@gmail.com>
2026-07-22 08:48:40 -0500

    feat: Mejorar manejo de excepciones y flujo en el registro y consulta de asistencias

--- JDSafe/interfaces/asistencias.py
+++ JDSafe/interfaces/asistencias.py
@@ -1,38 +1,64 @@
 from datetime import datetime
 from ..helpers import funciones_txt as funciones
 
-asistencia_y_observaciones = []
 print("Bienvenido al apartado de asistencias y observaciones \n")
 print("A continuacion se va a desplegar un menu que funciona con numeros \n")
 
 def registrar_asistencia_y_observacion():
-  cita = input("Ingrese el codigo de la cita \n")
-  existe = funciones.leer_archivo_txt("asistencias")
-  for asistencia in existe:
-    if asistencia["codigo"] == cita:
-        print("Ya existe una asistencia y observacion registrada para esta cita")
-        return
-  else:
-    fecha = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    observacion = input("Ingrese la observacion de la clase: \n" )
-    asistencia = {"codigo": cita, "fecha": fecha, "observacion": observacion}
-    asistencia_y_observaciones.append(asistencia)
-    funciones.crear_archivo_txt("asistencias",contenido=asistencia)
-    print("Asistencia y observacion registrada correctamente")
+  while True:
+    try:
+      cita = input("Ingrese el codigo de la cita \n")
+      existe = funciones.leer_archivo_txt("asistencias")
+      for asistencia in existe:
+        if asistencia["codigo"] == cita:
+            print("Ya existe una asistencia y observacion registrada para esta cita")
+            return
+      else:
+        fecha = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        observacion = input("Ingrese la observacion de la clase: \n" )
+
+        
+    except ValueError:
+      print("El dato ingresado no es valido, por favor ingrese un dato valido \n")
+    
+    except TypeError as e:
+      print(f"Se ha ingresado un dato no valido {e}\n")
+      
+    except Exception as e:
+      print(f"Se ha presentado un error inesperado {e}\n")
+      
+    else: 
+        asistencia = {"codigo": cita, "fecha": fecha, "observacion": observacion}
+        funciones.crear_archivo_txt("asistencias",contenido=asistencia)
+        print("Asistencia y observacion registrada correctamente")
+        
             
         
 def consultar_asistencia_y_observacion():
-  cita = input("Ingrese el codigo de la cita")
-  existe = funciones.leer_archivo_txt("asistencias")
-  if existe:
-    for asistencia in existe:
-      if asistencia["codigo"] == cita:
-        print(f"Codigo de cita: {asistencia['codigo']}")
-        print(f"Fecha: {asistencia['fecha']}")
-        print(f"Observacion: {asistencia['observacion']}")
-        return
-      else:
-        print("No se encontro una asistencia y observacion registrada para esta cita")
+  while True:
+   try:  
+    cita = input("Ingrese el codigo de la cita")
+    existe = funciones.leer_archivo_txt("asistencias")
+    if existe:
+      for asistencia in existe:
+        if asistencia["codigo"] == cita:
+          print(f"Codigo de cita: {asistencia['codigo']}")
+          print(f"Fecha: {asistencia['fecha']}")
+          print(f"Observacion: {asistencia['observacion']}")
+          return
+        else:
+          print("No se encontro una asistencia y observacion registrada para esta cita")
+    
+   except ValueError:
+      print("El dato ingresado no es valido, por favor ingrese un dato valido \n")
+
+   except TypeError as e:
+      print(f"Se ha ingresado un dato no valido {e}\n")
+      
+   except Exception as e:
+      print(f"Se ha presentado un error inesperado {e}\n")
+      
+
       
 
 while True:
