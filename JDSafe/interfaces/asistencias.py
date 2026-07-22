0c57bd506c8bc8e09e374ba2dac39775d877e2fc
JDEX22 <davidesparzaj22@gmail.com>
2026-07-22 13:31:13 -0500

    feat: Eliminar menú de opciones en el módulo de asistencias y observaciones

--- JDSafe/interfaces/asistencias.py
+++ JDSafe/interfaces/asistencias.py
@@ -1,9 +1,6 @@
 from datetime import datetime
 from ..helpers import funciones_txt as funciones
 
-print("Bienvenido al apartado de asistencias y observaciones \n")
-print("A continuacion se va a desplegar un menu que funciona con numeros \n")
-
 def registrar_asistencia_y_observacion():
   while True:
     try:
@@ -57,39 +54,7 @@ def consultar_asistencia_y_observacion():
       
    except Exception as e:
       print(f"Se ha presentado un error inesperado {e}\n")
-      
 
-      
-
-while True:
-  try:
-      print("1. Registrar asistencia y observacion")
-      print("2. Consultar asistencia y observacion")
-      print("Ingrese 0 para salir del apartado de asistencias y observaciones. Esto lo llevara al menu principal \n")
-
-      opcion = int(input("Ingrese la opcion: \n"))
-      
-      if opcion == 1:
-        registrar_asistencia_y_observacion()
-      
-      elif opcion == 2:
-        consultar_asistencia_y_observacion()
-            
-      elif opcion == 0:
-        print("Saliendo del apartado de asistencias y observaciones")
-        break
-    
-  except ValueError:
-      print("Opcion invalida. Por favor, ingrese un numero valido.")
-  
-  except KeyError:
-      print("Codigo de cita no encontrado. Por favor, ingrese un codigo valido.")
-      
-  except FileNotFoundError:
-      print("No se encontraron asistencias y observaciones registradas. Por favor, registre una asistencia y observacion primero.") 
-  
-  except Exception as e:
-      print(f"Ocurrio un error inesperado: {e}")
 
 
 
