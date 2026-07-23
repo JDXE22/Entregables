93aa594e55f44b491d3768abb875e79c94d49502
JuanDavid <davidesparzaj22@gmail.com>
2026-07-22 22:36:38 -0500

    fix: agregar impresión de detalles de asistencia registrada y mejorar la entrada del código de cita

--- JDSafe/interfaces/asistencias.py
+++ JDSafe/interfaces/asistencias.py
@@ -30,6 +30,11 @@ def registrar_asistencia_y_observacion():
         asistencia = {"codigo": cita, "fecha": fecha, "observacion": observacion}
         funciones.crear_archivo_txt("asistencias",contenido=asistencia)
         print("Asistencia y observacion registrada correctamente")
+        for asistencia in existe:
+          if asistencia["codigo"] == cita:
+            print(f"Codigo de cita: {asistencia['codigo']}")
+            print(f"Fecha: {asistencia['fecha']}")
+            print(f"Observacion: {asistencia['observacion']}")
         return
         
             
@@ -37,7 +42,7 @@ def registrar_asistencia_y_observacion():
 def consultar_asistencia_y_observacion():
   while True:
    try:  
-    cita = input("Ingrese el codigo de la cita")
+    cita = int(input("Ingrese el codigo de la cita recuerde que es un numero de 3 digitos: \n"))
     existe = funciones.leer_archivo_txt("asistencias")
     if existe:
       for asistencia in existe:
