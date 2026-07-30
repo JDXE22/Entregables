9967470e27af1c0b687e2a676e8480c2b01cd12a
JDEX22 <davidesparzaj22@gmail.com>
2026-07-30 16:04:07 -0500

    feat: agregar función para consultar evaluaciones de prácticas por cliente con validación de entrada

--- Examen_Modulo_Python/interfaces/evaluaciones.py
+++ Examen_Modulo_Python/interfaces/evaluaciones.py
@@ -101,6 +101,42 @@ def registrar_evaluacion():
     funciones.crear_archivo("evaluaciones", nueva_entrada)
     print("La evaluacion ha sido creada exitosamente")
 
+def consulta_estudiante():
+    evaluaciones = funciones.leer_archivo("evaluaciones")
+    print("Consultando evaluaciones de las practicas por cliente")
+    print("Ingrese el nombre del cliente para consultar sus evaluaciones")
+    while True:
+        try:
+            nombre_cliente = input("Ingrese el primer nombre y apellido del cliente").strip().replace(" ", "")
+            if nombre_cliente.isalpha():
+                tamano = funciones.calcular_tamaño(nombre_cliente)
+                if 6 <= tamano <= 50:
+                    nombre_cliente = nombre_cliente.upper() 
+                    print(f"Nombre válido. Bienvenido/a, {nombre_cliente}\n")
+                    encontradas = [cliente for cliente in evaluaciones if cliente['cliente'] == nombre_cliente]
+                    if encontradas:
+                        print(f"Se han encontrado los siguientes resultados del cliente: {nombre_cliente}")
+                        for cliente in encontradas:
+                            print(f"Nombre del cliente: {cliente['cliente']}, Nombre del instructor: {cliente['instructor']}, Fecha de la evaluacion: {cliente['fecha_evaluacion']}, Calificacion: {cliente['calificacion']}")
+                    else:
+                        print("No se encontraron citas para el cliente ingresado.\n")
+                        return
+                else:
+                    print(f"El nombre solo contiene letras, pero su longitud ({tamano}) debe estar entre 6 y 50 caracteres.")
+                    print("Intente de nuevo.\n")
+            else:
+                print("El nombre ingresado no es válido. No debe contener números ni símbolos.")
+                print("Intente de nuevo.\n")
+
+        except ValueError:
+            print("El nombre ingresado no es válido. Debe contener solo letras.")
+            print("Intente de nuevo.\n")
+            continue
+            
+        except Exception as e:
+            print(f"Se ha producido un error inesperado: {e}")
+            print("Intente de nuevo.\n")
+            continue
 
 
 
